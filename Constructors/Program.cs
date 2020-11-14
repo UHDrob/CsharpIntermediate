d12ab287400d4c1a334a0cf4357c881fb3c5c41c
Roberto Gomez <rob.77@live.com>
2020-11-13 19:31:34 -0600

    Constructors

---
+++ Constructors/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Constructors
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var customer = new Customer(1, "John");
+         Console.WriteLine(customer.Id);
+         Console.WriteLine(customer.Name);
+         Console.WriteLine();
+      }
+   }
+}
--- Intermediate/Program.cs
+++ Intermediate/Program.cs
@@ -2,11 +2,31 @@
 
 namespace Intermediate
 {
+   public class Person
+   {
+      public string Name;
+
+      public void Introduce(string to)
+      {
+         Console.WriteLine($"Hi {to}, my name is {Name}");
+      }
+
+      public static Person Parse(string str)
+      {
+         var person = new Person();
+         person.Name = str;
+
+         return person;
+      }
+   }
+
    class Program
    {
       static void Main(string[] args)
       {
-         Console.WriteLine("Hello World!");
+         var person = Person.Parse("John");
+         person.Introduce("Mosh");
       }
    }
+
 }

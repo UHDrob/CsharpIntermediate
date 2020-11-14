9ffdd3a9b52f664a836ce2e04cea97aef26e478b
Roberto Gomez <rob.77@live.com>
2020-11-14 10:24:14 -0600

    Methods

---
+++ Methods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Methods
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var point = new Point(10,20);
+         point.Move(new Point(40,60));
+         Console.WriteLine($"Point X {point.X} Point Y {point.Y} ");
+
+         point.Move(100, 200);
+         Console.WriteLine($"Point X {point.X} Point Y {point.Y} ");
+      }
+   }
+}
